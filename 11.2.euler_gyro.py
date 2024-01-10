f80311043c2aaa9fb6af411e41e0ebfd3702fe33
onesixx <onesixx@nate.com>
2024-01-10 18:13:34 +0900

    11

--- 11.2.euler_gyro.py
+++ 11.2.euler_gyro.py
@@ -4,6 +4,23 @@ import matplotlib.pyplot as plt
 from math import sin, cos, tan, pi
 from scipy import io
 
+'''
+오일러 각도의 변화율을 나타내는 식
+[phi,theta,psi] = [[1,  sin(phi)tan(theta), cos(phi)tan(theta)],
+                   [0,            cos(phi), -sin(theta)],
+                   [0, sin(phi)/cos(theta), cos(phi)/con(theta)]]@[p,q,r].T
+속도를 적분하면 위치를, 가속도를 적분하면 속도를 구할 수 있습니다. 
+이와 마찬가지로, 각속도를 적분하면 각도를 구할 수 있습니다.
+
+이를 적분하여 오일러 각도를 구하려면, 
+각 변화율에 대해 시간 간격(dt)을 곱한 후 
+이전 각도에 더하는 방식으로 수행할 수 있습니다.
+
+이 함수는 각속도(p, q, r)와 시간 간격(dt)을 입력으로 받아, 
+오일러 각도(phi, theta, psi)를 계산합니다. 
+계산된 각도는 전역 변수(prevPhi, prevTheta, prevPsi)에 저장되어, 다음 호출 시에 사용됩니다. 
+아래 함수 EulerGyro는 계산된 오일러 각도를 반환합니다.
+'''
 # 헬기의 Angular velocity 각속도(p, q, r)를 측정하여 오일러각 (phi, theta, psi)를 추정
 prevPhi, prevTheta, prevPsi = None,None,None
 
@@ -11,6 +28,7 @@ def EulerGyro(p,q,r,dt):
     """Calculate Euler angle (Pose Orientation)."""
     global prevPhi, prevTheta, prevPsi
     if prevPhi is None:
+        # 초기 오일러 각도를 0으로 설정 
         prevPhi   = 0
         prevTheta = 0
         prevPsi   = 0
@@ -24,9 +42,7 @@ def EulerGyro(p,q,r,dt):
     theta = prevTheta + dt*(    q*cosPhi          - r*sinPhi)
     psi   = prevPsi   + dt*(    q*sinPhi/cosTheta + r*cosPhi/cosTheta)
 
-    prevPhi   = phi
-    prevTheta = theta
-    prevPsi   = psi
+    prevPhi, prevTheta, prevPsi = phi, theta, psi
 
     return phi, theta, psi
 
@@ -40,12 +56,12 @@ def getGyro(i):
     r = input_mat['wz'][i][0]  # (41500, 1)
     return p, q, r
 
-
 n_samples = 41500
 dt = 0.01
 time = np.arange(0, n_samples*dt ,dt)
 
-EulerSaved = np.zeros([n_samples,3])
+## getGyro 관찰값 
+EulerSaved = np.zeros([n_samples, 3])  # [phi, theta, psi]
 for k in range(n_samples):
     p,q,r = getGyro(k)
     # phi, theta, psi = EulerGyro(p,q,r, dt)
@@ -57,25 +73,25 @@ for k in range(n_samples):
     phi, theta, psi = EulerGyro(p,q,r, dt)
     EulerSaved[k] = [phi, theta, psi]
 
+# 오일러 각도를 라디안에서 도(degree)로 변환
+PhiSaved   = EulerSaved[:,0] * 180/pi
+ThetaSaved = EulerSaved[:,1] * 180/pi
+PsiSaved   = EulerSaved[:,2] * 180/pi
+
 '''
 roll  -> phi
 pitch -> theta
 yaw   -> psi
 '''
-
-PhiSaved   = EulerSaved[:,0] * 180/pi
-ThetaSaved = EulerSaved[:,1] * 180/pi
-PsiSaved   = EulerSaved[:,2] * 180/pi
-
 plt.figure()
-plt.plot(time, PhiSaved)
-plt.xlabel('Time [Sec]')
+plt.plot(time/60, PhiSaved)
+plt.xlabel('Time [minites]')
 plt.ylabel('Roll ($\phi$) angle [deg]')
 # plt.savefig('result/11_EulerGyro_roll.png')
 
 plt.figure()
-plt.plot(time, ThetaSaved)
-plt.xlabel('Time [Sec]')
+plt.plot(time/60, ThetaSaved)
+plt.xlabel('Time [minites]')
 plt.ylabel('Pitch ($\\theta$) angle [deg]')
 # plt.savefig('result/11_EulerGyro_pitch.png')
 
@@ -85,3 +101,5 @@ plt.xlabel('Time [Sec]')
 plt.ylabel('Yaw ($\\psi$) angle [deg]')
 # plt.savefig('result/11_EulerGyro_pitch.png')
 plt.show()
+
+#자이로는 자세각 "측정"보다는 자세각의 "동태"를 측정하는 데 더 유용
